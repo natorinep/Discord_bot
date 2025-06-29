036ba1c6688b2db08d42dcffd1803fc7427d375f
Natori <nepneplus@gmail.com>
2025-06-29 23:46:13 +0900

    デバッグメッセージ追加

--- app/main.py
+++ app/main.py
@@ -13,11 +13,19 @@ intents.voice_states = True
 client = discord.Client(intents=intents)
 
 @client.event
+async def on_ready():
+    print(f'{client.user} がログインしました！')
+    print(f'サーバー数: {len(client.guilds)}')
+
+@client.event
 async def on_message(message):
+    print(f'メッセージ受信: {message.content} from {message.author}')
+    
     if message.author == client.user:
         return
 
     if message.content.startswith('$hello'):
+        print('$helloコマンド実行中...')
         await message.channel.send('Hello!')
 
 # Koyeb用 サーバー立ち上げ
