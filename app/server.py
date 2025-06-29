9cdc087708f5bc42eadff0b024327b393e888c2f
Natori <nepneplus@gmail.com>
2025-06-29 23:56:38 +0900

    POSTメソッドにも対応

--- app/server.py
+++ app/server.py
@@ -1,17 +1,17 @@
 from threading import Thread
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-	return {"message": "Server is Online."}
+@app.api_route("/", methods=["GET", "HEAD", "POST"])
+async def root(request: Request):
+    return {"message": "Server is Online.", "method": request.method}
 
 def start():
-	uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
 def server_thread():
-	t = Thread(target=start)
-	t.start()
+    t = Thread(target=start)
+    t.start()
